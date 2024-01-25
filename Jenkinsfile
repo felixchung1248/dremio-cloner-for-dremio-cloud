98c90393bb358a73c7bde0315233fee2e713af7a
Felix <felix.chung@felixchung.net>
2024-01-25 16:15:28 +0800

    add data catalog deploy

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,6 +4,7 @@ pipeline {
     parameters {
         string(name: 'FOLDER_PATH', description: 'Folder path of the dataset')
         string(name: 'DATASET_NAME', description: 'Dataset name')
+		string(name: 'BATCH_KEY', description: 'Unique ID for data catalog request')
     }
 
     environment {
@@ -21,6 +22,10 @@ pipeline {
                     }
                     if (!params.DATASET_NAME) {
                         error "The DATASET_NAME parameter is missing. Please provide the dataset name."
+                    }
+					
+					if (!params.BATCH_KEY) {
+                        error "The BATCH_KEY parameter is missing. Please provide the batch key."
                     }
                 }
             }
@@ -40,6 +45,25 @@ pipeline {
                                 python3 /app/workspace/replace_param.py json_file_path=config/config_read_dremio_cloud.json endpoint=${env.DREMIO_URL_SANDBOX} username=${env.DREMIO_USR_SANDBOX} password=${env.DREMIO_PW_SANDBOX} dremio_cloud_org_id=${env.DREMIO_CLOUD_ORG_ID_SANDBOX} dremio_cloud_project_id=${env.DREMIO_CLOUD_PROJECT_ID_SANDBOX} space.folder.filter='${env.FOLDER_PATH}' vds.filter.names='${env.DATASET_NAME}'
 								python3 /app/workspace/src/dremio_cloner.py /app/workspace/config/config_read_dremio_cloud.json_filled
 								python3 /app/workspace/src/dremio_cloner.py /app/workspace/config/config_write_dremio_cloud.json
+							   """
+                        }
+                    }
+                }
+            }
+        }
+		
+		stage('Deploy data catalog dataset') {
+            steps {
+                script {					
+					
+                    // Run the Python script within the Docker container
+                    docker.withRegistry("https://${env.DOCKER_REGISTRY}", env.DOCKER_REGISTRY_CREDENTIALS_ID) {
+                        // Create a Docker image object
+                        def pythonImage = docker.image("${env.DOCKER_REGISTRY}/${env.GOLDEN_PROJECT_NAME}/${env.GOLDEN_DOCKER_IMAGE}:${env.GOLDEN_DOCKER_TAG}")
+                        // Run the container with the script mounted and execute the Python script
+                        pythonImage.inside("-v ${env.WORKSPACE}:/app/workspace") {
+                            sh """
+                                python3 /app/workspace/data-catalog-deploy.py batch_key=${env.BATCH_KEY}
 							   """
                         }
                     }
---
+++ data-catalog-deploy.py
@@ -0,0 +1,132 @@
+# Inlined from /metadata-ingestion/examples/library/dataset_schema.py
+# Imports for urn construction utility methods
+from datahub.emitter.mce_builder import make_data_platform_urn, make_dataset_urn
+from datahub.emitter.mcp import MetadataChangeProposalWrapper
+from datahub.emitter.rest_emitter import DatahubRestEmitter
+
+import psycopg2
+import sys
+
+
+# Imports for metadata model classes
+from datahub.metadata.schema_classes import *
+
+
+# Database configuration - You might want to store these in environment variables
+db_config = {
+    # 'server': 'postgresql.postgresql-ns.svc.cluster.local',
+    'server': 'datamgmtdemo01.eastasia.cloudapp.azure.com',
+    'database': 'demodb01',
+    'username': 'felixchung',
+    'password': 'Passw0rd',
+    'port': '30432' 
+}
+
+def case1():
+    return StringTypeClass()
+
+def case2():
+    return BooleanTypeClass()
+
+def case3():
+    return NumberTypeClass()
+
+def case4():
+    return DateTypeClass()
+
+def case5():
+    return TimeTypeClass()
+
+def get_db_connection(db_config):
+    conn_string = f"dbname={db_config['database']} user={db_config['username']} password={db_config['password']} host={db_config['server']} port={db_config['port']}"
+    conn = psycopg2.connect(conn_string)
+    return conn
+
+# Check if at least one additional argument was passed (excluding the script name)
+if len(sys.argv) < 2:
+    print("Usage: python3 data-catalog-deploy.py batch_key=value")
+    sys.exit(1)
+# Extract the batch_key from the command line arguments
+batch_key_arg = sys.argv[1]
+if batch_key_arg.startswith('batch_key='):
+    batchKey = batch_key_arg.split('=', 1)[1]
+else:
+    print("The batch_key argument was not provided correctly.")
+    sys.exit(1)
+
+with get_db_connection(db_config) as conn:
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute("""
+                select 
+                    dataset_path
+                    , name
+                    , description
+                    , data_type
+                    , is_sensitive
+                from 
+                    DATA_CATALOG_DRAFT 
+                where batch_key=%s
+                """
+                ,(batchKey,)
+            )
+            # Fetch all the rows
+            rows = cursor.fetchall()
+            dataset_name = '"."' .join(rows[0][0].split('/'))
+            dataset_name = f'"{dataset_name}"'
+            switch_dict = {
+                        "VARCHAR": case1,
+                        "BOOLEAN": case2,
+                        "DECIMAL": case3,
+                        "INT": case3,
+                        "BIGINT": case3,
+                        "FLOAT": case3,
+                        "DOUBLE": case3,
+                        "DATE": case4,
+                        "TIME": case5,
+                        "TIMESTAMP": case5,
+                    }
+            
+            fields = []
+            for row in rows:
+                dataset_path = row[0]
+                name = row[1]
+                description = row[2]
+                data_type = row[3]
+                is_sensitive = row[4]
+                typeClass = switch_dict.get(data_type)()
+                field = SchemaFieldClass(
+                                fieldPath=name,
+                                type=SchemaFieldDataTypeClass(type=typeClass),
+                                nativeDataType=data_type,  # use this to provide the type of the field in the source system's vernacular
+                                description=description,
+                                lastModified=AuditStampClass(
+                                    time=1640692800000, actor="urn:li:corpuser:ingestion"
+                                ),
+                            )
+                fields.append(field)
+                
+            event: MetadataChangeProposalWrapper = MetadataChangeProposalWrapper(
+                entityUrn=make_dataset_urn(platform="dremio", name=dataset_name, env="PROD"),
+                aspect=SchemaMetadataClass(
+                    schemaName="customer",  # not used
+                    platform=make_data_platform_urn("dremio"),  # important <- platform must be an urn
+                    version=0,  # when the source system has a notion of versioning of schemas, insert this in, otherwise leave as 0
+                    hash="",  # when the source system has a notion of unique schemas identified via hash, include a hash, else leave it as empty string
+                    platformSchema=OtherSchemaClass(rawSchema="__insert raw schema here__"),
+                    lastModified=AuditStampClass(
+                        time=1640692800000, actor="urn:li:corpuser:ingestion"
+                    ),
+                    fields=fields,
+                ),
+            )
+            # Create rest emitter
+            rest_emitter = DatahubRestEmitter(gms_server="http://datamgmtdemo01.eastasia.cloudapp.azure.com:31080", token="eyJhbGciOiJIUzI1NiJ9.eyJhY3RvclR5cGUiOiJVU0VSIiwiYWN0b3JJZCI6ImRhdGFodWIiLCJ0eXBlIjoiUEVSU09OQUwiLCJ2ZXJzaW9uIjoiMiIsImp0aSI6Ijg0MDJiNTY2LTI1YzItNDJkMC05ODc5LWVhYjA0MzM4ODYwZSIsInN1YiI6ImRhdGFodWIiLCJpc3MiOiJkYXRhaHViLW1ldGFkYXRhLXNlcnZpY2UifQ.eliCy-PWOQGMC3PFjl-1UTlQjPhgamehTZ7KDzqisaU")
+            cursor.execute("update DATA_CATALOG_DRAFT set status = %s where batch_key=%s",('Completed',batchKey,))
+            rest_emitter.emit(event)
+            conn.commit()
+        except psycopg2.DatabaseError as e:
+            print(e)
+            conn.rollback()  # Rollback the transaction on error
+        finally:
+            print('process ended')
