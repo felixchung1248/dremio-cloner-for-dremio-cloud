4e0cf0763a5d9ab9a97746b67082457ea116e862
Felix <felix.chung@felixchung.net>
2024-02-01 18:36:48 +0800

    fix

--- replace_param.py
+++ replace_param.py
@@ -12,20 +12,24 @@ def parse_key_value_pairs(argv):
 # Function to recursively replace PARAM_ values with provided parameters in the JSON structure
 def replace_params(obj, params,json_file_path):
     dremio_cloner = obj['dremio_cloner']
-    source = dremio_cloner[1]['source']
-    source[0]['endpoint'] = params['endpoint']
-    source[1]['username'] = params['username']
-    source[2]['password'] = params['password']
-    source[7]['dremio_cloud_org_id'] = params['dremio_cloud_org_id']
-    source[8]['dremio_cloud_project_id'] = params['dremio_cloud_project_id']
-
     if "config_read_dremio_cloud.json" in json_file_path:
+        source = dremio_cloner[1]['source']
+        source[0]['endpoint'] = params['endpoint']
+        source[1]['username'] = params['username']
+        source[2]['password'] = params['password']
+        source[7]['dremio_cloud_org_id'] = params['dremio_cloud_org_id']
+        source[8]['dremio_cloud_project_id'] = params['dremio_cloud_project_id']   
         options = dremio_cloner[3]['options']
         options[22]['space.folder.filter'] = params['space.folder.filter']
-
         vds_filter_names = params['vds.filter.names'].split(',')
         options[35]['vds.filter.names'] = vds_filter_names
-
+    elif "config_write_dremio_cloud.json" in json_file_path:
+        target = dremio_cloner[1]['target']
+        target[0]['endpoint'] = params['endpoint']
+        target[1]['username'] = params['username']
+        target[2]['password'] = params['password']
+        target[6]['dremio_cloud_org_id'] = params['dremio_cloud_org_id']
+        target[7]['dremio_cloud_project_id'] = params['dremio_cloud_project_id'] 
 
 # Parse the command-line arguments
 parameters = parse_key_value_pairs(sys.argv)
